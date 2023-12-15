c82af500fa65a5180286c52557b2edf34f1bb081
Toni <antonioinsa@tutanota.com>
2023-12-15 02:09:58 +0100

    Feat:add all necesary library and prepare app for start the proyect

---
+++ src/common/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Header.css';
+
+export const Header = () => {
+    <div className='headerDesign'>
+        <>
+
+            <div>
+                home
+            </div>
+
+
+            <div>
+                about
+            </div>
+
+        </>
+    </div>
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import "./Home.css";
+
+export const Home = () => {
+    return (
+<div className="homeDesign">
+<h1>Hola home</h1>
+</div>
+    )
+}
