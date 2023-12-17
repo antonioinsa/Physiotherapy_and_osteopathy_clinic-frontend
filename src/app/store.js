f1de3250cacd6d0917bf39c556b6c2b10f927dcc
Toni <antonioinsa@tutanota.com>
2023-12-17 12:26:12 +0100

    Feat:update userSlice with reducers login, logout and role

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../pages/userSlice';
-// import searchSlice from '../pages/searchSlice';
-
 import { combineReducers } from 'redux';
-
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
-
 import thunk from 'redux-thunk';
 
 const reducers = combineReducers({
     user: userSlice,
-    // search: searchSlice
 })
 
 const persistConfig = {
--- src/pages/userSlice.js
+++ src/pages/userSlice.js
@@ -3,8 +3,12 @@ import { createSlice } from '@reduxjs/toolkit';
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
-      credentials: {}
+      credentials: {
+        token: "",
+        role: ""
+      },
     },
+    
     reducers: {
       login: (state, action) => {
         return {
@@ -17,13 +21,18 @@ export const userSlice = createSlice({
           ...state,
           ...action.payload
         }
-      }
-
+      },
+      role: (state, action) => {
+        return {
+          ...state,
+          ...action.payload
+        }
+      },
     }
-
+    
 });
 
-export const { login, logout } = userSlice.actions;
+export const { login, logout, role } = userSlice.actions;
 
 export const userData = (state) => state.user;
 
