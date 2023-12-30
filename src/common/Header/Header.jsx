3e77280e9d3b251cd4db8213c3db2c3217ce653a
Toni <antonioinsa@tutanota.com>
2023-12-30 10:33:59 +0100

    Feat:Modification of the navbar where, upon expanding the header, it enlarges its size, and upon selecting a menu option, it automatically contracts, and a transition is added to smooth the expansion and contraction of the header

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,126 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.css';
 import { Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { useLocation, useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router-dom";
 import { logout, userData } from "../../pages/userSlice";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
 
-
-
-
 export const Header = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const location = useLocation()
-
   const rdxCredentials = useSelector(userData)
+  const [navbarExpanded, setNavbarExpanded] = useState(false)
 
   const logOut = () => {
     dispatch(logout({ credentials: '', role: '' }))
     navigate('/')
   }
 
-  const home = () => {
-    navigate('/')
+  const handleNavbarToggle = () => {
+    setNavbarExpanded(!navbarExpanded)
   }
 
-  const about = () => {
-    navigate('/about')
-  }
-
-  const account = () => {
-    navigate('/account')
-  }
-
-  const appointments = () => {
-    navigate('/myAppointments')
-  }
-
-  const newAppointment = () => {
-    navigate('/newAppointment')
-  }
-
-  const manageWorkers = () => {
-    navigate('/saWorkers')
-  }
-
-  const manageClients = () => {
-    navigate('/saUsers')
-  }
-
-  const invoices = () => {
-    navigate('/myInvoices')
-  }
-
-  const mySpace = () => {
-    navigate('/workerSpace')
-  }
-
-  const saSpace = () => {
-    navigate('/saProfile')
-  }
-
-  const saInvoices = () => {
-    navigate('/saInvoices')
-  }
 
   return (
-    <Col sm={12} lg={6} xl={4} xxl={3} className="headerDesign">
-      <Navbar expand="lg" className="customNavBar">
+    <Col sm={12} lg={6} xl={4} xxl={3} className={`headerDesign ${navbarExpanded ? 'expanded' : ''}`}>
+      <Navbar expand="lg" className="customNavBar" onToggle={handleNavbarToggle} expanded={navbarExpanded}>
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand as={Link} to="/">
             <div className="insideLogoDesign">
-            <Image
-              className="logoImage"
-              src="../src/images/logo_mini.png"
-              alt="Pterion"
-            />
+              <Image
+                className="logoImage"
+                src="../src/images/logo_mini.png"
+                alt="Pterion"
+              />
             </div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav style={{ marginLeft: 'auto' }}>
+            <Nav className="ms-auto">
               {rdxCredentials?.credentials ? (
                 <>
-                  {rdxCredentials.role === 'user' && (
-                    <Nav.Link className="font-weight-bold" onClick={home}>Home</Nav.Link>
+                  {rdxCredentials.role !== 'admin' && rdxCredentials.role !== 'superAdmin' && (
+                    <Nav.Link className="font-weight-bold" as={Link} to="/" onClick={() => setNavbarExpanded(false)}>Home</Nav.Link>
+                  )}
+                  {rdxCredentials.role !== 'admin' && rdxCredentials.role !== 'superAdmin' && (
+                    <Nav.Link className="font-weight-bold" as={Link} to="/about" onClick={() => setNavbarExpanded(false)}>About</Nav.Link>
                   )}
                   {rdxCredentials.role === 'user' && (
-                    <Nav.Link className="font-weight-bold" onClick={about}>About</Nav.Link>
-                  )}
-                  {rdxCredentials.role === 'user' && (
-                    <Nav.Link className="font-weight-bold" onClick={account}>Account</Nav.Link>
+                    <Nav.Link className="font-weight-bold" as={Link} to="/account" onClick={() => setNavbarExpanded(false)}>Account</Nav.Link>
                   )}
                   {rdxCredentials.role === 'admin' && (
-                    <Nav.Link className="font-weight-bold" onClick={mySpace}>My Space</Nav.Link>
+                    <Nav.Link className="font-weight-bold" as={Link} to="/workerSpace" onClick={() => setNavbarExpanded(false)}>My Space</Nav.Link>
                   )}
                   {rdxCredentials.role === 'superAdmin' && (
-                    <Nav.Link className="font-weight-bold" onClick={saSpace}>My Space</Nav.Link>
+                    <Nav.Link className="font-weight-bold" as={Link} to="/saProfile" onClick={() => setNavbarExpanded(false)}>My Space</Nav.Link>
                   )}
                   {rdxCredentials.role === 'user' && (
-                    <Nav.Link className="font-weight-bold" onClick={appointments}>Appointments</Nav.Link>
+                    <Nav.Link className="font-weight-bold" as={Link} to="/myAppointments" onClick={() => setNavbarExpanded(false)}>Appointments</Nav.Link>
                   )}
                   {rdxCredentials.role === 'user' && (
-                    <Nav.Link className="font-weight-bold" onClick={newAppointment}>New Appointment</Nav.Link>
+                    <Nav.Link className="font-weight-bold" as={Link} to="/newAppointment" onClick={() => setNavbarExpanded(false)}>New Appointment</Nav.Link>
                   )}
                   {rdxCredentials.role === 'superAdmin' && (
                     <>
-                      <Nav.Link className="font-weight-bold" onClick={manageWorkers}>Manage Workers</Nav.Link>
-                      <Nav.Link className="font-weight-bold" onClick={manageClients}>Manage Clients</Nav.Link>
-                      <Nav.Link className="font-weight-bold" onClick={saInvoices}>Invoices</Nav.Link>
+                      <Nav.Link className="font-weight-bold" as={Link} to="/saWorkers" onClick={() => setNavbarExpanded(false)}>Manage Workers</Nav.Link>
+                      <Nav.Link className="font-weight-bold" as={Link} to="/saUsers" onClick={() => setNavbarExpanded(false)}>Manage Clients</Nav.Link>
+                      <Nav.Link className="font-weight-bold" as={Link} to="/saInvoices" onClick={() => setNavbarExpanded(false)}>Invoices</Nav.Link>
                     </>
                   )}
                   {rdxCredentials.role === 'user' && (
-                    <Nav.Link className="font-weight-bold" onClick={invoices}>Invoices</Nav.Link>
+                    <Nav.Link className="font-weight-bold" as={Link} to="/invoices" onClick={() => setNavbarExpanded(false)}>Invoices</Nav.Link>
                   )}
                   <Nav.Link className="font-weight-bold" onClick={logOut}>Log Out</Nav.Link>
                 </>
               ) : (
-                <Nav.Link className="font-weight-bold" href="/login">Login</Nav.Link>
+                <>
+                  {rdxCredentials.role !== 'admin' && rdxCredentials.role !== 'superAdmin' && (
+                    <Nav.Link className="font-weight-bold" as={Link} to="/" onClick={() => setNavbarExpanded(false)}>Home</Nav.Link>
+                  )}
+                  {rdxCredentials.role !== 'admin' && rdxCredentials.role !== 'superAdmin' && (
+                    <Nav.Link className="font-weight-bold" as={Link} to="/about" onClick={() => setNavbarExpanded(false)}>About</Nav.Link>
+                  )}
+                  <Nav.Link className="font-weight-bold" as={Link} to="/login" onClick={() => setNavbarExpanded(false)}>Login</Nav.Link>
+                </>
               )}
             </Nav>
           </Navbar.Collapse>
@@ -129,4 +94,3 @@ export const Header = () => {
     </Col>
   )
 }
-
